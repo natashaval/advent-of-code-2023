4c3e173b6cd2f36a46929f7e1be71e20f8fa22f5
natashaval <natasha.valentina1998@gmail.com>
2023-12-07 19:33:45 +0000

    2 part 1 because end of line

--- 2.py
+++ 2.py
@@ -2,30 +2,33 @@ text = open('2_input.txt', 'r')
 line_list = text.readlines()
 
 sum = 0
-for index, line in enumerate(line_list):
+for line in line_list:
+    line = line.replace(";", ",")
     after_game = line.split(":")[1]
-    turns = after_game.split(";")
+    game_id = int(line.split(":")[0].split(" ")[1])
+    balls = after_game.split(",")
     check = True
-    for turn_index, turn in enumerate(turns):
-        if check is False:
-            continue
+    for ball in balls:
+        # NEED to replace last \n
+        ball = ball[1:].replace("\n", "").replace("\r", "")
+        number = int(ball.split(" ")[0])
+        color = ball.split(" ")[1]
+        # print(f"ball {number} {color}")
+        if (color == "red" and number > 12) or (color == "green" and number > 13) or (color == "blue" and number > 14):
+            check = False
+            break
 
-        balls = turn.split(",")
-        for ball in balls:
-            ball = ball[1:]
-            number = int(ball.split(" ")[0])
-            color = ball.split(" ")[1]
-            # print(f"turn {turn_index}: ball {number} {color}")
-            if (color == "red" and number > 12) or (color == "green" and number > 13) or (color == "blue" and number > 14):
-                check = False
-                break
-        # print(f"=== turn {turn_index} check: {check}")
+    # print(f"=== turn {turn_index} check: {check}")
     if check is True:
         # print(f"+++ GAME {index + 1} last sum: {sum} \n")
-        sum +=  index + 1
+        sum +=  game_id
 
 print(sum)
 
 # Answer: 2619 (too high)
 
 # 1 - 25 correct
+# 37 - 50 different
+# 44 - 50 different
+# 44 - 47 DIFFERENT 47 vs 138
+# not 44, YES 45, YES 46 -> the issue is checking end of line !!!!
